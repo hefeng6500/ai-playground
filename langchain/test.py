2b31e0bfb5d4df3525523d889622a2cad42eed8a
兰亭古墨 <2443992009@qq.com>
2025-10-29 22:26:21 +0800

    feat: 添加DALL-E图像生成功能并更新天气应用文档
    refactor(test.py): 替换环境变量检查为DALL-E图像生成链
    docs: 添加新的消息文档并更新天气应用示例

--- langchain/test.py
+++ langchain/test.py
@@ -1,6 +1,13 @@
-import os
+from langchain.chains import LLMChain
+from langchain_community.utilities.dalle_image_generator import DallEAPIWrapper
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import OpenAI
 
-print(os.environ.get("ANTHROPIC_API_KEY"))
+llm = OpenAI(temperature=0.9)
+prompt = PromptTemplate(
+    input_variables=["image_desc"],
+    template="Generate a detailed prompt to generate an image based on the following description: {image_desc}",
+)
+chain = LLMChain(llm=llm, prompt=prompt)
 
-print(os.getenv("ANTHROPIC_API_KEY"))  # 应该能输出 key
-print(os.getenv("ANTHROPIC_PROXY"))  # 应该能输出 key
+image_url = DallEAPIWrapper().run(chain.run("halloween night at a haunted museum"))
